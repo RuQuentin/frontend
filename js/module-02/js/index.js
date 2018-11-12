06bdb1b7507b0f1c8e8a1320e989c28ad1aee785
RuQuenin <40791399+RuQuentin@users.noreply.github.com>
2018-11-12 22:20:10 +0300

    converts to separate functional blocks: variables, attempts, switching messages, displaying messages

--- js/module-02/js/index.js
+++ js/module-02/js/index.js
@@ -2,32 +2,33 @@
 
 const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 
-let userPass = prompt('Введите пароль...');
-let isCorrect = false;
+let userPassword;
+let isCorrectPassword;
 let message;
+const maxAttempts = 3;
 
-for (let counter = 2; counter >= 0; counter -= 1) {
-  let hasInput = userPass !== null;
+for (let counter = maxAttempts - 1; counter >= 0; counter -= 1) {
+  userPassword = prompt('Введите пароль...');
 
-  if (hasInput) {
-    /*     isCorrect = passwords.includes(userPass); */
-
-    for (let password of passwords) {
-      isCorrect = password === userPass;
-      if (isCorrect) break;
-    }
-
-    if (isCorrect) {
-      message = 'Добро пожаловать!';
-      break;
-    } else {
-      message = 'У Вас закончились попытки, аккаунт заблокирован!';
-      userPass = prompt(`Неверный пароль, у Вас осталось ${counter} попыток`);
-    }
+  if (userPassword !== null) {
+    isCorrectPassword = passwords.includes(userPassword);
+    if (isCorrectPassword) break;
+    if (counter > 0) alert(`Неверно. У Вас осталось ${counter} попыток`);
   } else {
-    message = 'Вы отменили вход. Заходите в другой раз.';
+    isCorrectPassword = null;
     break;
   }
+}
+
+switch (isCorrectPassword) {
+  case true:
+    message = 'Добро пожаловать!';
+    break;
+  case false:
+    message = 'У Вас закончились попытки, аккаунт заблокирован!';
+    break;
+  case null:
+    message = 'Вы отменили вход. Заходите в другой раз.';
 }
 
 alert(message);
