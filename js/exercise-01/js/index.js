a4f60ab696cfb32c5d24a5c017158b10b9eebe36
RuQuenin <40791399+RuQuentin@users.noreply.github.com>
2018-11-20 13:44:36 +0300

    change exercise folder name

---
+++ js/exercise-01/js/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const getUserInput = function() {
+  return prompt('Введите числа через запятую', '');
+};
+
+const convertStringToArray = function(string) {
+  let array = string.split(',');
+  return array;
+};
+
+const convertArrayToNumbers = function(inputArray) {
+  let outputArrayItem;
+  let outputArray = [];
+
+  for (let i = 0, max = inputArray.length; i < max; i += 1) {
+    outputArrayItem = Number(inputArray[i]);
+    if (!Number.isNaN(outputArrayItem)) outputArray.push(outputArrayItem);
+  }
+
+  return outputArray;
+};
+
+let userInput = getUserInput();
+let userArray = convertStringToArray(userInput);
+let userNumbers = convertArrayToNumbers(userArray);
+
+const findMin = function(item, minNumber) {
+  if (item < minNumber) {
+    minNumber = item;
+  }
+  return minNumber;
+};
+
+const findMax = function(item, maxNumber) {
+  if (item > maxNumber) {
+    maxNumber = item;
+  }
+  return maxNumber;
+};
+
+const findMaxMin = function(array, typeOfComparison) {
+  let result = array[0];
+
+  for (let i = 1, max = array.length; i < max; i += 1) {
+    result = typeOfComparison(array[i], result);
+  }
+
+  return result;
+};
+
+const testMin = findMaxMin(userNumbers, findMin);
+const testMax = findMaxMin(userNumbers, findMax);
+
+console.log('Исходные значения ' + userArray);
+console.log('Минимальное число ' + testMin);
+console.log('Максимальное число ' + testMax);
